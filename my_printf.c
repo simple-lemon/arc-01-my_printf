4896a7142b0d8881c61afb198a88df7d7db216a5
Qosimova Muminaxon <07janeokey07@gmail.com>
2023-12-25 14:54:36 +0500

    Testing

--- my_printf.c
+++ my_printf.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define BUFFER_SIZE 1024
-
-void my_printf(const char *format, ...) {
+int my_printf(char * restrict format, ...) {
     va_list args;
     va_start(args, format);
 
-    char buffer[BUFFER_SIZE];
-    int printed = vsnprintf(buffer, BUFFER_SIZE, format, args);
+    char *p = format;
+    while (*p != '\0') {
+        if (*p == '%') {
+            p++; 
+            switch (*p) {
+                case 'd':
+                    break;
 
-    if (printed >= BUFFER_SIZE) {
-        fprintf(stderr, "Error: Output exceeded buffer size.\n");
-    } else {
-        write(1, buffer, printed);
+                case 'o':
+                    break;
+
+                default:
+                    write(1, "%", 1);
+                    break;
+            }
+        } else {
+            write(1, p, 1);
+        }
+        p++;
     }
 
     va_end(args);
+    return 0;
 }
 
 int main() {

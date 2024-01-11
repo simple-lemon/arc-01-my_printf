4437caab5fe2974e1599cf7abf67e1ae6d5cc2f4
muhammadnurullayev <muhammadnurullaev10@gmail.com>
2024-01-11 18:13:51 +0500

    my printf

--- my_printf.c
+++ my_printf.c
@@ -1,216 +1,193 @@
 #include<unistd.h> 
 #include <stdarg.h>
 #include <stdio.h>
-int my_string(char* str){
+int my_len(char* arr){
     int i;
-    for(i = 0; i <= str[i];){
+    for(i = 0; i <= arr[i];){
         i++;
     }
     return i;
 }
-
-void my_cat(char hz) {
-    write(1,&hz,1);
+void my_param(char ab) {
+    write(1,&ab,1);
 }
-void my_char(char* str) {
-    while (*str) {
-        my_cat(*str);
-        str++;
+void my_hz(char* arr) {
+    while (*arr) {
+        my_param(*arr);
+        arr++;
     }
 }
-
-int my_ngram(unsigned long a1) {
-    my_char("0x");
-
+int my_string(unsigned long gcc) {
+    my_hz("0x");
     char place[20];
     int i = 0;
-
-    if (a1 == 0) {
-        my_cat('0');
+    if (gcc == 0) {
+        my_param('0');
         return 1;  
     }
-    
-    while (a1>0) {
-        int a2 = a1 % 16;
-        place[i++] = (a2 < 10) ? a2 + '0' : a2 - 10 + 'a';
-        a1 /= 16;
+    while (gcc>0) {
+        int cd = gcc % 16;
+        place[i++] = (cd < 10) ? cd + '0' : cd - 10 + 'a';
+        gcc /= 16;
     }
-    for (int k = i - 1; k >= 0; k--) {
-        my_cat(place[k]);
+    for (int j = i - 1; j >= 0; j--) {
+        my_param(place[j]);
     }
-
     return i + 2;  
 }
-
-int my_square(unsigned long a1) {
+int my_js(unsigned long gcc) {
     char place[20];
     int i = 0;
-
-    if (a1 == 0) {
-        my_cat('0');
+    if (gcc == 0) {
+        my_param('0');
         return 1;
     }
-
-    while(a1 > 0) {
-        int a2 = a1 % 16;
-        place[i++] = (a2 < 10) ? a2 + '0' : a2 - 10 + 'A';
-        a1 /= 16;
+    while(gcc > 0) {
+        int cd = gcc % 16;
+        place[i++] = (cd < 10) ? cd + '0' : cd - 10 + 'A';
+        gcc /= 16;
     }
-
-    for (int j = i - 1; j >= 0; j--) {
-        my_cat(place[j]);
+    for (int k = i - 1; k >= 0; k--) {
+        my_param(place[k]);
     }
-
     return i + 2;  
 }
-
-int my_file(unsigned int a1) {
+int my_cd(unsigned int gcc) {
     char place[20];
     int i = 0;
-
-    if (a1 == 0) {
-        my_cat('0');
+    if (gcc == 0) {
+        my_param('0');
         return 1; 
     }
-    for(i = 0;a1 > 0;) {
-        int a2 = a1 % 8;
-        place[i++] = a2 + '0';
-        a1 /= 8;
+    for(i = 0;gcc > 0;) {
+        int cd = gcc % 8;
+        place[i++] = cd + '0';
+        gcc /= 8;
     }
-    for (int k = i - 1; k >= 0; k--) {
-        my_cat(place[k]);
+    for (int a = i - 1; a >= 0; a--) {
+     my_param(place[a]);
     }
-
     return i;  
 }    
-void my_pointer(void* point) {
-    my_ngram((unsigned long)point);
+void my_masiv(void* len1) {
+    my_string((unsigned long)len1);
 }
-void my_cpewep(char* str, int length) {
-    int main = 0;
-    int end = length - 1;
-    while (main < end) {
-        char temp = str[main];
-        str[main] = str[end];
-        str[main] = temp;
-        main++;
-        end--;
+void my_down(char* hz, int param) {
+    int len = 0;
+    int def = param - 1;
+    while (len < def) {
+        char temp = hz[len];
+        hz[len] = hz[def];
+        hz[len] = temp;
+        len++;
+        def--;
     }
 }
-void my_str(int num, char* buffer) {
+void my_str(int sum, char* buffer) {
     int i = 0;
-    while (num != 0) {
-        buffer[i++] = num % 10 + '0';
-        num /= 10;
+    while (sum != 0) {
+        buffer[i++] = sum % 10 + '0';
+        sum /= 10;
     }
     buffer[i] = '\0';
-    my_cpewep(buffer, i);
+    my_down(buffer, i);
 }
-void my_git(double num, char* buffer, int precision) {
-    int int_part = (int)num;
-    double frac_part = num - int_part;
-
-    my_str(int_part, buffer);
-    int len = my_string(buffer);
-    buffer[len++] = '.';
+void my_add(double num, char* buffer, int precision) {
+    int part = (int)num;
+    double frac = num - part;
+    my_str(part, buffer);
+    int len2 = my_len(buffer);
+    buffer[len2++] = '.';
     int i = 0;
     while(i < precision) {
-        frac_part *= 10;
-        int digit = (int)frac_part;
-        buffer[len++] = digit + '0';
-        frac_part -= digit;
+        frac *= 10;
+        int digit = (int)frac;
+        buffer[len2++] = digit + '0';
+        frac -= digit;
         i++;
     }
-
-    buffer[len] = '\0';
+    buffer[len2] = '\0';
 }
-
-void my_add(double num, int precision) {
-    char place[50];
-    my_git(num, place, precision);
-    my_char(place);
+void my_int(double num2, int precision) {
+    char join[50];
+    my_add(num2, join, precision);
+    my_hz(join);
 }
-
-int my_push(int num) {
-    char place[22];  
+int my_gandalf(int num2) {
+    char join[22];  
     int i = 0;
-    int length = 0;  
-
-    if (num == 0) {
-        my_cat('0');
-        length++;  
+    int len3 = 0;
+    if (num2 == 0) {
+        my_param('0');
+        len3++;  
     }
-
-    if (num < 0) {
-        my_cat('-');
-        num = -num;
-        length++;  
+    if (num2 < 0) {
+        my_param('-');
+        num2 = -num2;
+        len3++;  
     }
-
-    for(i = 0;num > 0;) {
-        place[i++] = num % 10 + '0';
-        num /= 10;
+    for(i = 0;num2 > 0;) {
+        join[i++] = num2 % 10 + '0';
+        num2 /= 10;
     }
-    
-    for (int k = i - 1; k >= 0; k--) {
-        my_cat(place[k]);
-        length++;  
+    for (int b = i - 1; b >= 0; b--) {
+        my_param(join[b]);
+        len3++;  
     }
-
-    return length;  
+    return len3;  
 }
-int my_printf(char* hz, ...) {
+int my_printf(char* zed, ...) {
     va_list args;
-    va_start(args, hz);
-    int length = 0;
-    while (*hz) {
-        if (*hz == '%') {
-            hz++;
-            if (*hz == 'd' || *hz == 'i'){
-                int num = va_arg(args, int);
-                length += my_push(num);
+    va_start(args, zed);
+    int len4 = 0;
+    while (*zed) {
+        if (*zed == '%') {
+            zed++;
+            if (*zed == 'd' || *zed == 'i'){
+                int num2 = va_arg(args, int);
+                len4 += my_gandalf(num2);
             }
-            else if (*hz == 's'){
-                char* sss = va_arg(args, char*);
-               if (sss == NULL) {
-                   my_char("(null)");
-                   length += my_string("(null)");
+            else if (*zed == 's'){
+                char* abc = va_arg(args, char*);
+               if (abc == NULL) {
+                   my_hz("(null)");
+                   len4 += my_len("(null)");
                } else {
-                   my_char(sss);
-                   length += my_string(sss);
+                   my_hz(abc);
+                   len4 += my_len(abc);
                 }
             }       
-            else if (*hz == 'u'){
-                length += my_push(va_arg(args, int));
+            else if (*zed == 'u'){
+                len4 += my_gandalf(va_arg(args, int));
             }
-            else if (*hz == 'c'){
-                my_cat(va_arg(args, int));
-                length++;
+            else if (*zed == 'c'){
+                my_param(va_arg(args, int));
+                len4++;
             }
-            else if (*hz == 'p' || *hz == 'P'){
+            else if (*zed == 'p' || *zed == 'P'){
                 void* ptr = va_arg(args, void*);
-                my_ngram((unsigned long)ptr);
-                length += 14;
+                my_string((unsigned long)ptr);
+                len4 += 14;
             }
-            else if (*hz == 'x' || *hz == 'X') {
-                int x10lik = va_arg(args, int);
-                my_square((unsigned long)x10lik);
-                length += 1;
+            else if (*zed == 'x' || *zed == 'X') {
+                int x = va_arg(args, int);
+                my_js((unsigned long)x);
+                len4 += 1;
             }
-            else if (*hz == 'o' || *hz == 'O'){
-                length += my_file(va_arg(args, unsigned int));
+            else if (*zed == 'o' || *zed == 'O'){
+                len4 += my_cd(va_arg(args, unsigned int));
             }           
             }
-        else if (*hz == '\n') {
-            my_cat('\n');
-            length++; 
+        else if (*zed == '\n') {
+            my_param('\n');
+            len4++; 
         }    
         else {
-            my_cat(*hz);
-            length++; 
+            my_param(*zed);
+            len4++; 
         }
-        hz++;
+        zed++;
     }
     va_end(args);
-    return length;
+    return len4;
 }
